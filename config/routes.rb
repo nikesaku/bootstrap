41898810d7d8ed094cc6f0f4eaa1c56186ce575f
Yusaku Takada <nike@Nike-MacBook.local>
2018-01-21 05:24:19 +0900

    init

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       get :articles
     end
   end
+  get '/webpages', to: 'webpages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
