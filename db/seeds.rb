dbafcd2caea948962713985283d42f3ba53eda2e
Yusaku Takada <nikesaku@gmail.com>
2018-03-16 12:51:25 +0900

    画像うぷ機能実装直前

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,14 @@
+100.times do |n|
+  email = Faker::Internet.email
+  name = "name"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password,
+               )
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
